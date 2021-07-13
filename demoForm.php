6a62d2aef9de09b21e45f80ec72e3e1c6b4d2eef
Pradeepa Seelam <39674626+pradeepa45@users.noreply.github.com>
2021-07-13 13:51:25 +0530

    php test

---
+++ demoForm.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <script>
+        document.getElementById("DemoClassForm").style.display = "none";
+        document.getElementById("formDiv").innerHTML = "Thank you for submitting";
+    </script>
+</body>
+</hmtl>
